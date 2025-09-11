8d05d79ca9fd371e6d5789bf3bdd26d1b722661d
Farhah Alkaff <sharifahfarhah5@gmail.com>
2025-09-11 10:29:23 +1000

    setting seed

--- Helgoland_pro.R
+++ Helgoland_pro.R
@@ -181,17 +181,42 @@ ggplot(df_long, aes(month, z, color = variable, group = variable)) +
 ### Nitrate and fit models of different families ###
 ####################################################
 
+# create own df to remove NAs 
 Nitrate <- df %>% filter(!is.na(Nitrate)) %>% 
   select(Date, Nitrate)
 
+# plot that sucka
 ggplot(Nitrate, aes(x = Date, y = Nitrate)) +
   geom_line(color = "black", na.rm = TRUE) +
   labs(x = "Time", y = "Nitrate (µmol/l)") +
   theme_minimal()
 
-niSSTm <- gamlss(Nitrate ~ Date, family = SST(), data = na.omit(df), 
+
+
+### CHECK DISTRIBUTION ###
+
+# histogram
+hist(Nitrate$Nitrate)
+
+# density plot 
+ggplot(Nitrate, aes(y = Nitrate)) +
+  geom_density(
+    linewidth = 0.8
+  ) +
+  labs(
+    x = "Density",
+    y = "Value"
+  ) +
+  theme_minimal(base_size = 14) +
+  coord_flip()
+
+
+### MODELS ###
+
+niSSTm <- gamlss(Nitrate ~ Date, sigma.fo = ~ Date, nu.fo = ~ Date, family = SST(), data = na.omit(df), 
              method = mixed(5, 200),
              control = gamlss.control(n.cyc = 400, c.crit = 0.01, trace = FALSE))
+
 
 niTF2m <- gamlss(Nitrate ~ Date, family = TF2(), data = na.omit(df), 
              method = mixed(5, 200),
@@ -205,41 +230,140 @@ niNOm <- gamlss(Nitrate ~ Date, family = NO(), data = na.omit(df),
              method = mixed(5, 200),
              control = gamlss.control(n.cyc = 400, c.crit = 0.01, trace = FALSE))
 
-summary(m1)
-summary(m2)
 
-AIC(niSSTm)
+summary(niSSTm)
+summary(niTF2m)
 AIC(niTF2m)
-AIC(niSN1m)
-AIC(niNOm)
+AIC(niTF2mtest)
 
+
+niSSTm_1 <- gamlss(log(Nitrate) ~ Date, family = SST(), data = na.omit(df), 
+                 method = mixed(5, 200),
+                 control = gamlss.control(n.cyc = 400, c.crit = 0.01, trace = FALSE))
+summary(niSSTm_1)
+with(Nitrate, plot(Nitrate ~ Date))
+curve(exp(cbind(1,x)%*%coef(niSSTm_1)), add =T, col = "red", lwd=2)
+
+
+niSSTm_sig <- gamlss(log(Nitrate) ~ Date, sigma.fo = ~ Date, family = SST(), data = na.omit(df), 
+                   method = mixed(5, 200),
+                   control = gamlss.control(n.cyc = 400, c.crit = 0.01, trace = FALSE))
+summary(niSSTm_sig)
+
+niSSTm_nu <- gamlss(log(Nitrate) ~ Date, sigma.fo = ~ Date, nu.fo = ~ Date, family = SST(), data = na.omit(df), 
+                     method = mixed(5, 200),
+                     control = gamlss.control(n.cyc = 400, c.crit = 0.01, trace = FALSE))
+summary(niSSTm_nu)
+
+niSSTm_tau <- gamlss(log(Nitrate) ~ Date, sigma.fo = ~ Date, nu.fo = ~ Date, tau.fo = ~ Date, family = SST(), 
+                     data = na.omit(df), 
+                    method = mixed(5, 200),
+                    control = gamlss.control(n.cyc = 400, c.crit = 0.01, trace = FALSE))
+summary(niSSTm_tau)
+
+
+# summary
+param_summary <- data.frame(
+  param = c("mu","mu(time)" , "SD","SD(time)","nu", "nu(time)", "tau", "tau(time)", "AIC"), 
+  niSSTm_1 = c(coefAll(niSSTm_1)$mu[1],coefAll(niSSTm_1)$mu[2] ,exp(coefAll(niSSTm_1)$sigma),"NA", 
+                exp(coefAll(niSSTm_1)$nu),"NA", (exp(coefAll(niSSTm_1)$tau) + 2), "NA", AIC(niSSTm_1)),
+  niSSTm_sig = c(coefAll(niSSTm_sig)$mu[1],coefAll(niSSTm_sig)$mu[2] ,exp(coefAll(niSSTm_sig)$sigma[1]),
+                 exp(coefAll(niSSTm_sig)$sigma[2]), exp(coefAll(niSSTm_sig)$nu),"NA", 
+                 (exp(coefAll(niSSTm_sig)$tau) + 2), "NA", AIC(niSSTm_sig)),
+  niSSTm_nu = c(coefAll(niSSTm_nu)$mu[1],coefAll(niSSTm_nu)$mu[2], exp(coefAll(niSSTm_nu)$sigma[1]),
+                 exp(coefAll(niSSTm_nu)$sigma[2]), exp(coefAll(niSSTm_nu)$nu[1]),exp(coefAll(niSSTm_nu)$nu[2]), 
+                 (exp(coefAll(niSSTm_nu)$tau) + 2), "NA", AIC(niSSTm_nu)),
+  niSSTm_tau = c(coefAll(niSSTm_tau)$mu[1],coefAll(niSSTm_tau)$mu[2], exp(coefAll(niSSTm_tau)$sigma[1]),
+                exp(coefAll(niSSTm_tau)$sigma[2]), exp(coefAll(niSSTm_tau)$nu[1]),exp(coefAll(niSSTm_tau)$nu[2]), 
+                (exp(coefAll(niSSTm_tau)$tau[1]) + 2), (exp(coefAll(niSSTm_tau)$tau[2]) + 2), AIC(niSSTm_tau))
+)
+print(param_summary)
+
+
+# 
+# n <- 6000
+# t_index  <- seq_len(n)
+# t_scaled <- (t_index - 1)/(n - 1)
+# mu0 <- 0
+# mu_t <- pmax(0, mu0 + 0.005 * n)
+# sigma0 <- 4
+# sigma_t <- pmax(0, sigma0 + 1 * n)
+# nu0 <- 20
+# nu_t <- pmax(0, nu0 + 1 * n)
+# tau0 <- 2.1
+# 
+# y <- rSST(n, mu = mu_t, sigma = sigma_t, nu = nu0, tau = tau0)
+# test <- data.frame(
+#   t = t_index,
+#   t_scaled = n,
+#   y = y,
+#   sd_true = sigma_t,
+#   mu_true = mu_t,
+#   nu_true = nu0,
+#   tau_true = tau0
+# )
+#   
+# ggplot(test, aes(x = t)) +
+#   geom_line(
+#     aes(y = y),
+#     color = "darkred",
+#     linewidth = 0.8
+#   ) +
+#   labs(
+#     x = "Time",
+#     y = "Value"
+#   ) +
+#   theme_minimal(base_size = 14)
 
 ######################################################
 ### Phosphate and fit models of different families ###
 ######################################################
 
+# create own df to remove NAs
 bluh <- df %>% filter(!is.na(Phosphate)) %>% 
   select(Date, Phosphate)
 
-
+# plot that sucka
 ggplot(bluh, aes(x = Date, y = Phosphate)) +
   geom_line(color = "black") +
   labs(x = "Time", y = "Phosphate (µmol/l)") +
   theme_minimal()
 
-phSSTm <- gamlss(Phosphate ~ poly(Date,2), family = SST(), data = bluh, 
+
+### CHECK DISTRIBUTION ###
+
+# histogram 
+hist(bluh$Phosphate)
+
+# density plot 
+ggplot(bluh, aes(y = Phosphate)) +
+  geom_density(
+    linewidth = 0.8
+  ) +
+  labs(
+    x = "Density",
+    y = "Value"
+  ) +
+  theme_minimal(base_size = 14) +
+  coord_flip()
+
+
+### MODELS ###
+
+phSSTm <- gamlss(Phosphate ~ poly(Date,2), family = SST(), data = na.omit(df), 
+                 sigma.start = max(sd(df$Phosphate, na.rm = TRUE), 1e-6),
              method = mixed(5, 200),
              control = gamlss.control(n.cyc = 400, c.crit = 0.01, trace = FALSE))
 
-phTF2m <- gamlss(Phosphate ~ poly(Date,2), family = TF2(), data = na.omit(df), 
+phTF2m <- gamlss(Phosphate ~ Date, family = TF2(), data = na.omit(df), 
              method = mixed(5, 200),
              control = gamlss.control(n.cyc = 400, c.crit = 0.01, trace = FALSE))
 
-phSN1m <- gamlss(Phosphate ~ poly(Date,2), family = SN1(), data = na.omit(df), 
+phSN1m <- gamlss(Phosphate ~ Date, family = SN1(), data = na.omit(df), 
              method = mixed(5, 200),
              control = gamlss.control(n.cyc = 400, c.crit = 0.01, trace = FALSE))
 
-phNOm <- gamlss(Phosphate ~ poly(Date,1), family = NO(), data = na.omit(df), 
+phNOm <- gamlss(Phosphate ~ Date, family = NO(), data = na.omit(df), 
              method = mixed(5, 200),
              control = gamlss.control(n.cyc = 400, c.crit = 0.01, trace = FALSE))
 
@@ -254,15 +378,36 @@ AIC(phNOm)
 ### Nitrite and fit models of different families ###
 ####################################################
 
+# Create own df to remove NAs
 Nitrite <- df %>% filter(!is.na(Nitrite)) %>% 
   select(Date, Nitrite)
 
-
+# plot that sucka
 ggplot(Nitrite, aes(x = Date, y = Nitrite)) +
   geom_line(color = "black") +
   labs(x = "Time", y = "Nitrite (µmol/l)") +
   theme_minimal()
 
+
+### CHECK DISTRIBUTION ###
+
+# histogram
+hist(Nitrite$Nitrite)
+
+# density plot 
+ggplot(Nitrite, aes(y = Nitrite)) +
+  geom_density(
+    linewidth = 0.8
+  ) +
+  labs(
+    x = "Density",
+    y = "Value"
+  ) +
+  theme_minimal(base_size = 14) +
+  coord_flip()
+
+
+### MODELS ###
 
 niiSSTm <- gamlss(Nitrite ~ Date, family = SST(), data = Nitrite, 
                  method = mixed(5, 200),
@@ -292,15 +437,36 @@ AIC(niiNOm)
 ### DIN and fit models of different families ###
 ################################################
 
+# create it's own df to remove NAs
 DIN <- df %>% filter(!is.na(DIN)) %>% 
   select(Date, DIN)
 
-
+# plot that sucka
 ggplot(DIN, aes(x = Date, y = DIN)) +
   geom_line(color = "black") +
   labs(x = "Time", y = "DIN (µmol/l)") +
   theme_minimal()
 
+
+### CHECK DISTRIBUTION ###
+
+# histogram 
+hist(DIN$DIN)
+
+# density plot 
+ggplot(DIN, aes(y = DIN)) +
+  geom_density(
+    linewidth = 0.8
+  ) +
+  labs(
+    x = "Density",
+    y = "Value"
+  ) +
+  theme_minimal(base_size = 14) +
+  coord_flip()
+
+
+### MODELS ###
 
 DINSSTm <- gamlss(DIN ~ Date, family = SST(), data = DIN, 
                   method = mixed(5, 200),
@@ -330,15 +496,36 @@ AIC(DINNOm)
 ### Silicate and fit models of different families ###
 #####################################################
 
+# create own df to remove NAs 
 Silicate <- df %>% filter(!is.na(Silicate)) %>% 
   select(Date, Silicate)
 
-
+# plot that sucka 
 ggplot(Silicate, aes(x = Date, y = Silicate)) +
   geom_line(color = "black") +
   labs(x = "Time", y = "Silicate (µmol/l)") +
   theme_minimal()
 
+
+### CHECK DISTRIBUTION ###
+
+# histogram 
+hist(Silicate$Silicate)
+
+# look at the density plot 
+ggplot(Ammonium, aes(y = Ammonium)) +
+  geom_density(
+    linewidth = 0.8
+  ) +
+  labs(
+    x = "Density",
+    y = "Value"
+  ) +
+  theme_minimal(base_size = 14) +
+  coord_flip()
+
+
+### MODELS ###
 
 siSSTm <- gamlss(Silicate ~ Date, family = SST(), data = Silicate, 
                   method = mixed(5, 200),
@@ -357,6 +544,7 @@ siNOm <- gamlss(Silicate ~ Date, family = NO(), data = Silicate,
                  control = gamlss.control(n.cyc = 400, c.crit = 0.01, trace = FALSE))
 
 
+summary(siSSTm)
 AIC(siSSTm)
 AIC(siTF2m)
 AIC(siSN1m)
@@ -368,15 +556,36 @@ AIC(siNOm)
 ### Ammonium and fit models of different families ###
 #####################################################
 
+# create own df to remove NAs 
 Ammonium <- df %>% filter(!is.na(Ammonium)) %>% 
   select(Date, Ammonium)
 
-
+# plot that sucka
 ggplot(Ammonium, aes(x = Date, y = Ammonium)) +
   geom_line(color = "black") +
   labs(x = "Time", y = "Ammonium (µmol/l)") +
   theme_minimal()
 
+
+### CHECK DISTRIBUTION ###
+
+# histogram
+hist(Ammonium$Ammonium)
+
+# look at the density plot 
+ggplot(Ammonium, aes(y = Ammonium)) +
+  geom_density(
+    linewidth = 0.8
+  ) +
+  labs(
+    x = "Density",
+    y = "Value"
+  ) +
+  theme_minimal(base_size = 14) +
+  coord_flip()
+
+
+### MODELS ###
 
 amSSTm <- gamlss(Ammonium ~ Date, family = SST(), data = Ammonium, 
                  method = mixed(5, 200),
